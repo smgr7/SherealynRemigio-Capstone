6446154ed3a639e26ce7235c414aec93f5f8e53e
smgr7 <48784504+smgr7@users.noreply.github.com>
2025-06-03 14:35:11 +0800

    Capstone Part 1

---
+++ SherealynRemigio-Capstone/src/StockForm.jsx
@@ -0,0 +1,26 @@
+import './StockFormStyling.css'
+
+function StockForm() {
+
+    return (
+    <>
+         <form>
+            <label>
+            <input name='stockSymbol' type='text' placeholder='Stock Symbol'/>
+            </label>
+
+            <label>
+            <input name='quantity' type='number' placeholder='Quantity'/>
+            </label>
+
+            <label>
+            <input name='purchasePrice' type='number' placeholder='Purchase Price'/>
+            </label>
+
+            <button type='submit'>Add Stock</button>
+        </form>
+     </>
+    )   
+}
+
+export default StockForm;
